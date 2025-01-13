dd6315e4b5b3d88bf9c2f1eeee2fe50b21dff6d2
Nicolás Foti <nicolasdfoti@gmail.com>
2025-01-13 17:29:48 -0300

    Added JS

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,3 +1,34 @@
+// First Part
 let input = document.querySelector("#favchap");
 let button = document.querySelector("button");
 let list = document.querySelector("#list");
+
+// Second Part
+
+button.addEventListener("click", function() {
+
+    if (input.value.trim() === "") {
+        input.focus();
+    }
+
+    else if (input.value.trim() != "") {
+        const titleLi = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        titleLi.textContent = input.value;
+        deleteButton.textContent = "X";
+
+        titleLi.append(deleteButton);
+
+        list.append(titleLi);
+
+        input.value = "";
+        input.focus();
+
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(titleLi);
+            input.focus();
+        })
+
+    }
+})

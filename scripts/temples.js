c8168adbf8205d0c60550956e59240d86cbb176f
Nicolás Foti <nicolasdfoti@gmail.com>
2025-01-14 01:13:39 -0300

    Corrected JS

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,9 +2,7 @@
 
 const year = document.querySelector("#current-year");
 const lastModified = document.querySelector("#lastModified");
-
 const today = new Date();
 
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
-
-lastModified.innerHTML = `<span class="highlight">${document.lastModified}</span>`;
+lastModified.innerHTML = `<span class="highlight">${document.lastModified}</span>`

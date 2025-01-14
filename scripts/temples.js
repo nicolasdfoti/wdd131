9fc1ad2802feb56f4b13a437e80017dacda6f600
Nicolás Foti <nicolasdfoti@gmail.com>
2025-01-14 01:41:05 -0300

    Added JS

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,14 @@ const today = new Date();
 
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 lastModified.innerHTML = `<span class="highlight">${document.lastModified}</span>`
+
+
+// Second Part:
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
